328ac1e46e2e5abfe4e83ec29f2ba6075065d6a9
Akshat Nigam <akshat.dev.zero@gmail.com>
2022-01-10 21:58:41 +0530

    Added tests for multiple deposits.

--- test/gateway/Gateway.test.ts
+++ test/gateway/Gateway.test.ts
@@ -27,15 +27,20 @@ describe("Gateway Tests", function () {
   let walletFactory: WalletFactory;
   let usd: MockContract<USD>;
 
+  const ALICE_IDENTIFIER = keccak("1");
   const BOB_IDENTIFIER = keccak("2");
+  const EVE_IDENTIFIER = keccak("3");
   const _0 = bigNumberify("0");
   const _1_000 = bigNumberify("1000000000");
+  const _2_000 = bigNumberify("2000000000");
+  const _3_000 = bigNumberify("3000000000");
+  const _4_000 = bigNumberify("4000000000");
   const _5_000 = bigNumberify("5000000000");
+  const _5_001 = bigNumberify("5001000000");
   const _100_000 = bigNumberify("100000000000");
   const _200_000 = bigNumberify("200000000000");
   const _300_000 = bigNumberify("300000000000");
   const _400_000 = bigNumberify("400000000000");
-  const _500_000 = bigNumberify("500000000000");
 
   before(async function () {
     [deployer, owner, treasury, alice, bob, eve] = await ethers.getSigners();
@@ -81,7 +86,7 @@ describe("Gateway Tests", function () {
   });
 
   describe("processes transactions", function () {
-    it("bob sends 5000 USD tokens", async function () {
+    it("bob deposits 5000 USD tokens", async function () {
       const identifier = BOB_IDENTIFIER;
       const bytecode = Wallet__factory.bytecode;
       const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
@@ -103,15 +108,92 @@ describe("Gateway Tests", function () {
         .to.emit(walletFactory, "WalletCreated")
         .withArgs(identifier, walletAddress, bigNumberify(1));
 
-      // Owner transfers funds from Bob's smart-wallet to treasury
-      await expect(walletFactory.connect(owner).transferFrom(identifier, usd.address, _5_000))
+      // Owner fails to transfer amount exceeding balance from Bob's smart-wallet to treasury
+      await expect(walletFactory.connect(owner).transferFrom(identifier, usd.address, _5_001)).to.be.revertedWith(
+        "ERC20: transfer amount exceeds balance",
+      );
+
+      // Owner can transfer less amount from Bob's smart-wallet to treasury
+      await expect(walletFactory.connect(owner).transferFrom(identifier, usd.address, _3_000))
         .to.emit(usd, "Transfer")
-        .withArgs(walletAddress, treasuryAddress, _5_000);
+        .withArgs(walletAddress, treasuryAddress, _3_000);
+
+      // Balances
+      expect(await usd.balanceOf(bobAddress)).to.equal(_200_000.sub(_5_000));
+      expect(await usd.balanceOf(treasuryAddress)).to.equal(_3_000);
+      expect(await usd.balanceOf(walletAddress)).to.equal(_2_000);
+
+      // Owner transfers whole amount from Bob's smart-wallet to treasury
+      await expect(walletFactory.connect(owner).transferFrom(identifier, usd.address, _2_000))
+        .to.emit(usd, "Transfer")
+        .withArgs(walletAddress, treasuryAddress, _2_000);
 
       // Final Balances
       expect(await usd.balanceOf(bobAddress)).to.equal(_200_000.sub(_5_000));
       expect(await usd.balanceOf(treasuryAddress)).to.equal(_5_000);
       expect(await usd.balanceOf(walletAddress)).to.equal(_0);
+    });
+
+    it("alice & eve deposit USD tokens", async function () {
+      const bytecode = Wallet__factory.bytecode;
+      const aliceWallet = getCreate2Address(walletFactory.address, ALICE_IDENTIFIER, bytecode);
+      const eveWallet = getCreate2Address(walletFactory.address, EVE_IDENTIFIER, bytecode);
+
+      // Unique Wallets
+      expect(aliceWallet).to.not.equal(eveWallet);
+
+      // Balances
+      expect(await usd.balanceOf(aliceAddress)).to.equal(_100_000);
+      expect(await usd.balanceOf(eveAddress)).to.equal(_300_000);
+      expect(await usd.balanceOf(treasuryAddress)).to.equal(_0);
+      expect(await usd.balanceOf(aliceWallet)).to.equal(_0);
+      expect(await usd.balanceOf(eveWallet)).to.equal(_0);
+
+      // Alice sends USD 3000 tokens to a pre-deterministic smart-contract wallet address
+      await expect(usd.connect(alice).transfer(aliceWallet, _3_000))
+        .to.emit(usd, "Transfer")
+        .withArgs(aliceAddress, aliceWallet, _3_000);
+      expect(await usd.balanceOf(aliceAddress)).to.equal(_100_000.sub(_3_000));
+      expect(await usd.balanceOf(aliceWallet)).to.equal(_3_000);
+
+      // Eve sends USD 1000 tokens to a pre-deterministic smart-contract wallet address
+      await expect(usd.connect(eve).transfer(eveWallet, _1_000))
+        .to.emit(usd, "Transfer")
+        .withArgs(eveAddress, eveWallet, _1_000);
+      expect(await usd.balanceOf(eveAddress)).to.equal(_300_000.sub(_1_000));
+      expect(await usd.balanceOf(eveWallet)).to.equal(_1_000);
+
+      // After detecting Alice's deposit, owner creates smart-wallet for Alice at the pre-determined address
+      await expect(walletFactory.connect(owner).createWallet(ALICE_IDENTIFIER))
+        .to.emit(walletFactory, "WalletCreated")
+        .withArgs(ALICE_IDENTIFIER, aliceWallet, bigNumberify(1));
+
+      // After detecting Eve's deposit, owner creates smart-wallet for Eve at the pre-determined address
+      await expect(walletFactory.connect(owner).createWallet(EVE_IDENTIFIER))
+        .to.emit(walletFactory, "WalletCreated")
+        .withArgs(EVE_IDENTIFIER, eveWallet, bigNumberify(2));
+
+      // Owner transfers funds from Alice's smart-wallet to treasury
+      await expect(walletFactory.connect(owner).transferFrom(ALICE_IDENTIFIER, usd.address, _3_000))
+        .to.emit(usd, "Transfer")
+        .withArgs(aliceWallet, treasuryAddress, _3_000);
+
+      // Balances
+      expect(await usd.balanceOf(aliceAddress)).to.equal(_100_000.sub(_3_000));
+      expect(await usd.balanceOf(treasuryAddress)).to.equal(_3_000);
+      expect(await usd.balanceOf(aliceWallet)).to.equal(_0);
+
+      // Owner transfers funds from Eve's smart-wallet to treasury
+      await expect(walletFactory.connect(owner).transferFrom(EVE_IDENTIFIER, usd.address, _1_000))
+        .to.emit(usd, "Transfer")
+        .withArgs(eveWallet, treasuryAddress, _1_000);
+
+      // Final Balances
+      expect(await usd.balanceOf(aliceAddress)).to.equal(_100_000.sub(_3_000));
+      expect(await usd.balanceOf(eveAddress)).to.equal(_300_000.sub(_1_000));
+      expect(await usd.balanceOf(treasuryAddress)).to.equal(_4_000);
+      expect(await usd.balanceOf(aliceWallet)).to.equal(_0);
+      expect(await usd.balanceOf(eveWallet)).to.equal(_0);
     });
   });
 });
--- test/gateway/WalletFactory.test.ts
+++ test/gateway/WalletFactory.test.ts
@@ -13,86 +13,211 @@ describe("WalletFactory Unit Tests", function () {
   let deployer: Signer;
   let owner: Signer;
   let treasury: Signer;
+  let other: Signer;
 
   let deployerAddress: string;
   let ownerAddress: string;
   let treasuryAddress: string;
+  let otherAddress: string;
 
   let walletFactory: WalletFactory;
 
+  const ZERO_ADDRESS = ethers.constants.AddressZero;
   const USER_IDENTIFIER_1 = keccak("1");
   const USER_IDENTIFIER_2 = keccak("2");
+  const _1_000 = bigNumberify("1000000000");
 
   before(async function () {
-    [deployer, owner, treasury] = await ethers.getSigners();
-    [deployerAddress, ownerAddress, treasuryAddress] = await Promise.all([
+    [deployer, owner, treasury, other] = await ethers.getSigners();
+    [deployerAddress, ownerAddress, treasuryAddress, otherAddress] = await Promise.all([
       deployer.getAddress(),
       owner.getAddress(),
       treasury.getAddress(),
+      other.getAddress(),
     ]);
-
-    // Deploy WalletFactory
-    const walletFactory__factory = await new WalletFactory__factory(deployer);
-    walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-      ownerAddress,
-      treasuryAddress,
-    ])) as WalletFactory;
-    await walletFactory.deployed();
   });
 
-  describe("validations", function () {
-    it("has an owner", async function () {
-      expect(await walletFactory.owner()).to.equal(ownerAddress);
+  describe("initialization", () => {
+    it("can be initialized and deployed", async () => {
+      const walletFactory__factory = await new WalletFactory__factory(deployer);
+      walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+        ownerAddress,
+        treasuryAddress,
+      ])) as WalletFactory;
+      await walletFactory.deployed();
+    });
+
+    it("will not allow a 0x0 Owner address", async () => {
+      const walletFactory__factory = await new WalletFactory__factory(deployer);
+      await expect(upgrades.deployProxy(walletFactory__factory, [ZERO_ADDRESS, treasuryAddress])).to.be.reverted;
+    });
+
+    it("will not allow a 0x0 Treasury address", async () => {
+      const walletFactory__factory = await new WalletFactory__factory(deployer);
+      await expect(upgrades.deployProxy(walletFactory__factory, [ownerAddress, ZERO_ADDRESS])).to.be.reverted;
     });
   });
 
-  describe("createWallet", function () {
-    it("can only be called by owner", async function () {
-      await expect(walletFactory.connect(deployer).createWallet(USER_IDENTIFIER_1)).to.be.revertedWith(
-        "Ownable: caller is not the owner",
-      );
+  describe("post-initialization", function () {
+    describe("validations", function () {
+      before(async function () {
+        const walletFactory__factory = await new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          ownerAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
+
+      it("has an owner", async function () {
+        expect(await walletFactory.owner()).to.equal(ownerAddress);
+      });
     });
 
-    it("owner creates a new wallet", async function () {
-      const identifier = USER_IDENTIFIER_1;
-      const bytecode = Wallet__factory.bytecode;
-      const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
+    describe("create wallet", function () {
+      before(async function () {
+        const walletFactory__factory = await new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          ownerAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
 
-      await expect(walletFactory.connect(owner).createWallet(identifier))
-        .to.emit(walletFactory, "WalletCreated")
-        .withArgs(identifier, walletAddress, bigNumberify(1));
-      await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
-      expect(await walletFactory.getWallet(identifier)).to.eq(walletAddress);
-      expect(await walletFactory.allWallets(0)).to.eq(walletAddress);
-      expect(await walletFactory.totalWallets()).to.eq(1);
+      it("can only be called by owner", async function () {
+        await expect(walletFactory.connect(deployer).createWallet(USER_IDENTIFIER_1)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+      });
 
-      const wallet = new Wallet__factory(deployer).attach(walletAddress);
-      expect(await wallet.owner()).to.eq(walletFactory.address);
+      it("owner creates a new wallet", async function () {
+        const identifier = USER_IDENTIFIER_1;
+        const bytecode = Wallet__factory.bytecode;
+        const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
 
-      await expect(wallet.connect(deployer).initialize(deployerAddress)).to.be.revertedWith(
-        "Initializable: contract is already initialized",
-      );
+        await expect(walletFactory.connect(owner).createWallet(identifier))
+          .to.emit(walletFactory, "WalletCreated")
+          .withArgs(identifier, walletAddress, bigNumberify(1));
+        await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
+        expect(await walletFactory.getWallet(identifier)).to.eq(walletAddress);
+        expect(await walletFactory.allWallets(0)).to.eq(walletAddress);
+        expect(await walletFactory.totalWallets()).to.eq(1);
+
+        const wallet = new Wallet__factory(deployer).attach(walletAddress);
+        expect(await wallet.owner()).to.eq(walletFactory.address);
+
+        await expect(wallet.connect(deployer).initialize(deployerAddress)).to.be.revertedWith(
+          "Initializable: contract is already initialized",
+        );
+      });
+
+      it("owner creates another new wallet", async function () {
+        const identifier = USER_IDENTIFIER_2;
+        const bytecode = Wallet__factory.bytecode;
+        const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
+
+        await expect(walletFactory.connect(owner).createWallet(identifier))
+          .to.emit(walletFactory, "WalletCreated")
+          .withArgs(identifier, walletAddress, bigNumberify(2));
+        await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
+        expect(await walletFactory.getWallet(identifier)).to.eq(walletAddress);
+        expect(await walletFactory.allWallets(1)).to.eq(walletAddress);
+        expect(await walletFactory.totalWallets()).to.eq(2);
+
+        const wallet = new Wallet__factory(deployer).attach(walletAddress);
+        expect(await wallet.owner()).to.eq(walletFactory.address);
+
+        await expect(wallet.connect(deployer).initialize(deployerAddress)).to.be.revertedWith(
+          "Initializable: contract is already initialized",
+        );
+      });
     });
 
-    it("owner creates another new wallet", async function () {
-      const identifier = USER_IDENTIFIER_2;
-      const bytecode = Wallet__factory.bytecode;
-      const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
+    describe("transfer ownership", function () {
+      beforeEach(async function () {
+        const walletFactory__factory = await new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          ownerAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
 
-      await expect(walletFactory.connect(owner).createWallet(identifier))
-        .to.emit(walletFactory, "WalletCreated")
-        .withArgs(identifier, walletAddress, bigNumberify(2));
-      await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
-      expect(await walletFactory.getWallet(identifier)).to.eq(walletAddress);
-      expect(await walletFactory.allWallets(1)).to.eq(walletAddress);
-      expect(await walletFactory.totalWallets()).to.eq(2);
+      it("changes owner after transfer", async function () {
+        await expect(walletFactory.connect(owner).transferOwnership(otherAddress))
+          .to.emit(walletFactory, "OwnershipTransferred")
+          .withArgs(ownerAddress, otherAddress);
+        expect(await walletFactory.owner()).to.equal(otherAddress);
+      });
 
-      const wallet = new Wallet__factory(deployer).attach(walletAddress);
-      expect(await wallet.owner()).to.eq(walletFactory.address);
+      it("prevents non-owners from transferring", async function () {
+        await expect(walletFactory.connect(deployer).transferOwnership(otherAddress)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+        await expect(walletFactory.connect(other).transferOwnership(otherAddress)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+      });
 
-      await expect(wallet.connect(deployer).initialize(deployerAddress)).to.be.revertedWith(
-        "Initializable: contract is already initialized",
-      );
+      it("guards ownership against stuck state", async function () {
+        await expect(walletFactory.connect(owner).transferOwnership(ZERO_ADDRESS)).to.be.revertedWith(
+          "Ownable: new owner is the zero address",
+        );
+      });
+    });
+
+    describe("update treasury", function () {
+      beforeEach(async function () {
+        const walletFactory__factory = await new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          ownerAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
+
+      it("changes treasury after update", async function () {
+        await expect(walletFactory.connect(owner).updateTreasury(otherAddress))
+          .to.emit(walletFactory, "TreasuryUpdated")
+          .withArgs(treasuryAddress, otherAddress);
+        expect(await walletFactory.treasury()).to.equal(otherAddress);
+      });
+
+      it("prevents non-owners from updating", async function () {
+        await expect(walletFactory.connect(deployer).updateTreasury(otherAddress)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+        await expect(walletFactory.connect(other).updateTreasury(otherAddress)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+      });
+
+      it("guards treasury against stuck state", async function () {
+        await expect(walletFactory.connect(owner).updateTreasury(ZERO_ADDRESS)).to.be.revertedWith(
+          "WalletFactory: new treasury is the zero address",
+        );
+      });
+    });
+
+    describe("transfer from", function () {
+      beforeEach(async function () {
+        const walletFactory__factory = await new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          ownerAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
+
+      it("prevents non-owners from transferring", async function () {
+        const identifier = USER_IDENTIFIER_1;
+        await expect(walletFactory.connect(deployer).transferFrom(identifier, otherAddress, _1_000)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+        await expect(walletFactory.connect(other).transferFrom(identifier, otherAddress, _1_000)).to.be.revertedWith(
+          "Ownable: caller is not the owner",
+        );
+      });
     });
   });
 });
