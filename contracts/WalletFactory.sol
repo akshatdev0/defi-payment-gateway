987581c536f2623eb73926433fd5d61e5c756f2c
Akshat Nigam <akshat.dev.zero@gmail.com>
2022-01-27 14:50:48 +0530

    Audit#1 Vulnerabilities fixed.

--- contracts/WalletFactory.sol
+++ contracts/WalletFactory.sol
@@ -1,24 +1,32 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 import "./Wallet.sol";
 
-contract WalletFactory is Ownable {
+contract WalletFactory is AccessControl {
+    bytes32 public constant APP_ROLE = keccak256("APP_ROLE");
+
     address public treasury;
 
     event WalletCreated(bytes32 identifier, address indexed walletAddress);
     event TreasuryUpdated(address indexed oldTreasury, address indexed newTreasury);
 
-    constructor(address owner_, address treasury_) {
-        require(owner_ != address(0), "WalletFactory: new owner is the zero address");
+    constructor(
+        address admin_,
+        address app_,
+        address treasury_
+    ) {
+        require(admin_ != address(0), "WalletFactory: new admin is the zero address");
+        require(app_ != address(0), "WalletFactory: new app is the zero address");
         require(treasury_ != address(0), "WalletFactory: new treasury is the zero address");
-        _transferOwnership(owner_);
-        _updateTreasury(treasury_);
+        _grantRole(DEFAULT_ADMIN_ROLE, admin_);
+        _grantRole(APP_ROLE, app_);
+        treasury = treasury_;
     }
 
-    function createWallet(bytes32 identifier) external onlyOwner {
+    function createWallet(bytes32 identifier) external onlyRole(APP_ROLE) {
         Wallet wallet = new Wallet{ salt: identifier }();
         wallet.initialize(address(this));
         emit WalletCreated(identifier, address(wallet));
@@ -28,16 +36,12 @@ contract WalletFactory is Ownable {
         address wallet,
         address token,
         uint256 amount
-    ) external onlyOwner returns (bool) {
+    ) external onlyRole(APP_ROLE) returns (bool) {
         return Wallet(wallet).transfer(token, treasury, amount);
     }
 
-    function updateTreasury(address newTreasury) public virtual onlyOwner {
+    function updateTreasury(address newTreasury) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(newTreasury != address(0), "WalletFactory: new treasury is the zero address");
-        _updateTreasury(newTreasury);
-    }
-
-    function _updateTreasury(address newTreasury) internal virtual {
         address oldTreasury = treasury;
         treasury = newTreasury;
         emit TreasuryUpdated(oldTreasury, newTreasury);
--- migrations/2_deploy_wallet_factory.js
+++ migrations/2_deploy_wallet_factory.js
@@ -3,9 +3,10 @@ const tronWeb = require("tronweb");
 var WalletFactory = artifacts.require("./WalletFactory.sol");
 
 module.exports = function (deployer) {
-  const owner = "TMTGdKkfxZr4dPX8cZQoBct9awB8CMRVdW";
+  const admin = "TMTGdKkfxZr4dPX8cZQoBct9awB8CMRVdW";
+  const app = "TMTGdKkfxZr4dPX8cZQoBct9awB8CMRVdW";
   const treasury = "TNQJYxt7ujpdo7xXg8DA15qBTxBSXZFp5L";
-  deployer.deploy(WalletFactory, owner, treasury).then(function () {
+  deployer.deploy(WalletFactory, admin, app, treasury).then(function () {
     return console.log(
       `WalletFactory deployed at address ${tronWeb.address.fromHex(WalletFactory.address)} (${WalletFactory.address})`,
     );
