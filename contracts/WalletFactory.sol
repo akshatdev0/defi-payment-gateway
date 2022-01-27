4596c9750b84ef84f7fd28f23a94617d005e11ea
Akshat Nigam <akshat.dev.zero@gmail.com>
2022-01-27 12:24:10 +0530

    Used AccessControl from openzeppelin instead of Ownable.

--- contracts/WalletFactory.sol
+++ contracts/WalletFactory.sol
@@ -2,25 +2,33 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 import "./Wallet.sol";
 
-contract WalletFactory is Initializable, OwnableUpgradeable {
+contract WalletFactory is Initializable, AccessControlUpgradeable {
+    bytes32 public constant APP_ROLE = keccak256("APP_ROLE");
+
     address public treasury;
 
     event WalletCreated(bytes32 identifier, address indexed walletAddress);
     event TreasuryUpdated(address indexed oldTreasury, address indexed newTreasury);
 
-    function initialize(address owner_, address treasury_) public initializer {
-        require(owner_ != address(0), "WalletFactory: new owner is the zero address");
+    function initialize(
+        address admin_,
+        address app_,
+        address treasury_
+    ) public initializer {
+        require(admin_ != address(0), "WalletFactory: new admin is the zero address");
+        require(app_ != address(0), "WalletFactory: new app is the zero address");
         require(treasury_ != address(0), "WalletFactory: new treasury is the zero address");
-        OwnableUpgradeable.__Ownable_init();
-        _transferOwnership(owner_);
-        _updateTreasury(treasury_);
+        AccessControlUpgradeable.__AccessControl_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin_);
+        _grantRole(APP_ROLE, app_);
+        treasury = treasury_;
     }
 
-    function createWallet(bytes32 identifier) external onlyOwner {
+    function createWallet(bytes32 identifier) external onlyRole(APP_ROLE) {
         Wallet wallet = new Wallet{ salt: identifier }();
         wallet.initialize(address(this));
         emit WalletCreated(identifier, address(wallet));
@@ -30,16 +38,12 @@ contract WalletFactory is Initializable, OwnableUpgradeable {
         address wallet,
         address token,
         uint256 amount
-    ) external onlyOwner returns (bool) {
+    ) external onlyRole(APP_ROLE) returns (bool) {
         return Wallet(wallet).transfer(token, treasury, amount);
     }
 
-    function updateTreasury(address newTreasury) public virtual onlyOwner {
+    function updateTreasury(address newTreasury) public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
         require(newTreasury != address(0), "WalletFactory: new treasury is the zero address");
-        _updateTreasury(newTreasury);
-    }
-
-    function _updateTreasury(address newTreasury) internal virtual {
         address oldTreasury = treasury;
         treasury = newTreasury;
         emit TreasuryUpdated(oldTreasury, newTreasury);
--- test/WalletFactory.test.ts
+++ test/WalletFactory.test.ts
@@ -13,17 +13,23 @@ chai.use(smock.matchers);
 describe("WalletFactory Unit Tests", function () {
   // Accounts
   let deployer: Signer;
-  let owner: Signer;
+  let admin: Signer;
+  let app: Signer;
   let treasury: Signer;
   let other: Signer;
+  let authorized: Signer;
 
   let deployerAddress: string;
-  let ownerAddress: string;
+  let adminAddress: string;
+  let appAddress: string;
   let treasuryAddress: string;
   let otherAddress: string;
+  let authorizedAddress: string;
 
   let walletFactory: WalletFactory;
 
+  const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  const APP_ROLE = keccak("APP_ROLE");
   const ZERO_ADDRESS = ethers.constants.AddressZero;
   const USER_IDENTIFIER_1 = keccak("1");
   const USER_IDENTIFIER_2 = keccak("2");
@@ -31,12 +37,14 @@ describe("WalletFactory Unit Tests", function () {
   const INIT_CODE_HASH = "0x79b8071417ed7109c7e1d6474ccec39a75b4578c3a526aedd173f5536b17d0ea";
 
   before(async function () {
-    [deployer, owner, treasury, other] = await ethers.getSigners();
-    [deployerAddress, ownerAddress, treasuryAddress, otherAddress] = await Promise.all([
+    [deployer, admin, app, treasury, other, authorized] = await ethers.getSigners();
+    [deployerAddress, adminAddress, appAddress, treasuryAddress, otherAddress, authorizedAddress] = await Promise.all([
       deployer.getAddress(),
-      owner.getAddress(),
+      admin.getAddress(),
+      app.getAddress(),
       treasury.getAddress(),
       other.getAddress(),
+      authorized.getAddress(),
     ]);
   });
 
@@ -44,28 +52,36 @@ describe("WalletFactory Unit Tests", function () {
     it("can be deployed and initialized only once", async () => {
       const walletFactory__factory = new WalletFactory__factory(deployer);
       walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-        ownerAddress,
+        adminAddress,
+        appAddress,
         treasuryAddress,
       ])) as WalletFactory;
       await walletFactory.deployed();
 
-      await expect(walletFactory.connect(deployer).initialize(ownerAddress, treasuryAddress)).to.be.revertedWith(
-        "Initializable: contract is already initialized",
-      );
+      await expect(
+        walletFactory.connect(deployer).initialize(adminAddress, appAddress, treasuryAddress),
+      ).to.be.revertedWith("Initializable: contract is already initialized");
     });
 
-    it("will not allow a 0x0 Owner address", async () => {
+    it("will not allow a 0x0 admin address", async () => {
       const walletFactory__factory = new WalletFactory__factory(deployer);
-      await expect(upgrades.deployProxy(walletFactory__factory, [ZERO_ADDRESS, treasuryAddress])).to.be.revertedWith(
-        "WalletFactory: new owner is the zero address",
-      );
+      await expect(
+        upgrades.deployProxy(walletFactory__factory, [ZERO_ADDRESS, appAddress, treasuryAddress]),
+      ).to.be.revertedWith("WalletFactory: new admin is the zero address");
+    });
+
+    it("will not allow a 0x0 app address", async () => {
+      const walletFactory__factory = new WalletFactory__factory(deployer);
+      await expect(
+        upgrades.deployProxy(walletFactory__factory, [adminAddress, ZERO_ADDRESS, treasuryAddress]),
+      ).to.be.revertedWith("WalletFactory: new app is the zero address");
     });
 
     it("will not allow a 0x0 Treasury address", async () => {
       const walletFactory__factory = new WalletFactory__factory(deployer);
-      await expect(upgrades.deployProxy(walletFactory__factory, [ownerAddress, ZERO_ADDRESS])).to.be.revertedWith(
-        "WalletFactory: new treasury is the zero address",
-      );
+      await expect(
+        upgrades.deployProxy(walletFactory__factory, [adminAddress, appAddress, ZERO_ADDRESS]),
+      ).to.be.revertedWith("WalletFactory: new treasury is the zero address");
     });
   });
 
@@ -74,14 +90,19 @@ describe("WalletFactory Unit Tests", function () {
       beforeEach(async function () {
         const walletFactory__factory = new WalletFactory__factory(deployer);
         walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-          ownerAddress,
+          adminAddress,
+          appAddress,
           treasuryAddress,
         ])) as WalletFactory;
         await walletFactory.deployed();
       });
 
-      it("has an owner", async function () {
-        expect(await walletFactory.owner()).to.equal(ownerAddress);
+      it("admin has the 'DEFAULT_ADMIN_ROLE'", async function () {
+        expect(await walletFactory.hasRole(DEFAULT_ADMIN_ROLE, adminAddress)).to.be.true;
+      });
+
+      it("app has the 'APP_ROLE'", async function () {
+        expect(await walletFactory.hasRole(APP_ROLE, appAddress)).to.be.true;
       });
 
       describe("create2", () => {
@@ -105,11 +126,11 @@ describe("WalletFactory Unit Tests", function () {
 
           generatedAddress = getCreate2Address(walletFactory.address, USER_IDENTIFIER_1, bytecode);
           log(walletFactory.address, USER_IDENTIFIER_1, INIT_CODE_HASH, generatedAddress);
-          expect(generatedAddress).to.be.equal("0xD20Cbc95BDab4c016E71317D23065B6b17bEcab3");
+          expect(generatedAddress).to.be.equal("0x8CdA19052d7DF0364adebE24CA9fb14723a5D771");
 
           generatedAddress = getCreate2Address(walletFactory.address, USER_IDENTIFIER_2, bytecode);
           log(walletFactory.address, USER_IDENTIFIER_2, INIT_CODE_HASH, generatedAddress);
-          expect(generatedAddress).to.be.equal("0x82348545F7944ac0B0Ff74104aFc6B7a290E72d2");
+          expect(generatedAddress).to.be.equal("0x30Fc161731F80f08C316B6995bfb99cCa1a3c44E");
 
           generatedAddress = getCreate2Address(deployerAddress, USER_IDENTIFIER_1, bytecode);
           log(deployerAddress, USER_IDENTIFIER_1, INIT_CODE_HASH, generatedAddress);
@@ -122,31 +143,82 @@ describe("WalletFactory Unit Tests", function () {
       });
     });
 
+    describe("access control", function () {
+      beforeEach(async function () {
+        const walletFactory__factory = new WalletFactory__factory(deployer);
+        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
+          adminAddress,
+          appAddress,
+          treasuryAddress,
+        ])) as WalletFactory;
+        await walletFactory.deployed();
+      });
+
+      describe("default admin", function () {
+        it("admin has the 'DEFAULT_ADMIN_ROLE'", async function () {
+          expect(await walletFactory.hasRole(DEFAULT_ADMIN_ROLE, adminAddress)).to.equal(true);
+        });
+
+        it("other roles's admin is the 'DEFAULT_ADMIN_ROLE'", async function () {
+          expect(await walletFactory.getRoleAdmin(APP_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
+        });
+
+        it("DEFAULT_ADMIN_ROLE's admin is itself", async function () {
+          expect(await walletFactory.getRoleAdmin(DEFAULT_ADMIN_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
+        });
+      });
+
+      describe("granting", function () {
+        it("admin can grant 'APP_ROLE' to other accounts", async function () {
+          await expect(walletFactory.connect(admin).grantRole(APP_ROLE, authorizedAddress))
+            .to.emit(walletFactory, "RoleGranted")
+            .withArgs(APP_ROLE, authorizedAddress, adminAddress);
+        });
+
+        it("non-admin cannot grant 'APP_ROLE' to other accounts", async function () {
+          await expect(walletFactory.connect(other).grantRole(APP_ROLE, authorizedAddress)).to.be.revertedWith(
+            `AccessControl: account ${otherAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+          );
+        });
+
+        it("accounts can be granted a role multiple times", async function () {
+          await expect(walletFactory.connect(admin).grantRole(APP_ROLE, authorizedAddress))
+            .to.emit(walletFactory, "RoleGranted")
+            .withArgs(APP_ROLE, authorizedAddress, adminAddress);
+          await expect(walletFactory.connect(admin).grantRole(APP_ROLE, authorizedAddress)).to.not.emit(
+            walletFactory,
+            "RoleGranted",
+          );
+        });
+      });
+    });
+
     describe("create wallet", function () {
       before(async function () {
         const walletFactory__factory = new WalletFactory__factory(deployer);
         walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-          ownerAddress,
+          adminAddress,
+          appAddress,
           treasuryAddress,
         ])) as WalletFactory;
         await walletFactory.deployed();
       });
 
-      it("can only be called by owner", async function () {
-        await expect(walletFactory.connect(deployer).createWallet(USER_IDENTIFIER_1)).to.be.revertedWith(
-          "Ownable: caller is not the owner",
+      it("can only be called by app", async function () {
+        await expect(walletFactory.connect(other).createWallet(USER_IDENTIFIER_1)).to.be.revertedWith(
+          `AccessControl: account ${otherAddress.toLowerCase()} is missing role ${APP_ROLE}`,
         );
       });
 
-      it("owner creates a new wallet", async function () {
+      it("app creates a new wallet", async function () {
         const identifier = USER_IDENTIFIER_1;
         const bytecode = Wallet__factory.bytecode;
         const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
 
-        await expect(walletFactory.connect(owner).createWallet(identifier))
+        await expect(walletFactory.connect(app).createWallet(identifier))
           .to.emit(walletFactory, "WalletCreated")
           .withArgs(identifier, walletAddress);
-        await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
+        await expect(walletFactory.connect(app).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
 
         const wallet = new Wallet__factory(deployer).attach(walletAddress);
         expect(await wallet.owner()).to.eq(walletFactory.address);
@@ -156,54 +228,21 @@ describe("WalletFactory Unit Tests", function () {
         );
       });
 
-      it("owner creates another new wallet", async function () {
+      it("app creates another new wallet", async function () {
         const identifier = USER_IDENTIFIER_2;
         const bytecode = Wallet__factory.bytecode;
         const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
 
-        await expect(walletFactory.connect(owner).createWallet(identifier))
+        await expect(walletFactory.connect(app).createWallet(identifier))
           .to.emit(walletFactory, "WalletCreated")
           .withArgs(identifier, walletAddress);
-        await expect(walletFactory.connect(owner).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
+        await expect(walletFactory.connect(app).createWallet(identifier)).to.be.reverted; // WALLET_EXISTS
 
         const wallet = new Wallet__factory(deployer).attach(walletAddress);
         expect(await wallet.owner()).to.eq(walletFactory.address);
 
         await expect(wallet.connect(deployer).initialize(deployerAddress)).to.be.revertedWith(
           "Initializable: contract is already initialized",
-        );
-      });
-    });
-
-    describe("transfer ownership", function () {
-      beforeEach(async function () {
-        const walletFactory__factory = new WalletFactory__factory(deployer);
-        walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-          ownerAddress,
-          treasuryAddress,
-        ])) as WalletFactory;
-        await walletFactory.deployed();
-      });
-
-      it("changes owner after transfer", async function () {
-        await expect(walletFactory.connect(owner).transferOwnership(otherAddress))
-          .to.emit(walletFactory, "OwnershipTransferred")
-          .withArgs(ownerAddress, otherAddress);
-        expect(await walletFactory.owner()).to.equal(otherAddress);
-      });
-
-      it("prevents non-owners from transferring", async function () {
-        await expect(walletFactory.connect(deployer).transferOwnership(otherAddress)).to.be.revertedWith(
-          "Ownable: caller is not the owner",
-        );
-        await expect(walletFactory.connect(other).transferOwnership(otherAddress)).to.be.revertedWith(
-          "Ownable: caller is not the owner",
-        );
-      });
-
-      it("guards ownership against stuck state", async function () {
-        await expect(walletFactory.connect(owner).transferOwnership(ZERO_ADDRESS)).to.be.revertedWith(
-          "Ownable: new owner is the zero address",
         );
       });
     });
@@ -212,14 +251,15 @@ describe("WalletFactory Unit Tests", function () {
       beforeEach(async function () {
         const walletFactory__factory = new WalletFactory__factory(deployer);
         walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-          ownerAddress,
+          adminAddress,
+          appAddress,
           treasuryAddress,
         ])) as WalletFactory;
         await walletFactory.deployed();
       });
 
       it("changes treasury after update", async function () {
-        await expect(walletFactory.connect(owner).updateTreasury(otherAddress))
+        await expect(walletFactory.connect(admin).updateTreasury(otherAddress))
           .to.emit(walletFactory, "TreasuryUpdated")
           .withArgs(treasuryAddress, otherAddress);
         expect(await walletFactory.treasury()).to.equal(otherAddress);
@@ -227,15 +267,24 @@ describe("WalletFactory Unit Tests", function () {
 
       it("prevents non-owners from updating", async function () {
         await expect(walletFactory.connect(deployer).updateTreasury(otherAddress)).to.be.revertedWith(
-          "Ownable: caller is not the owner",
+          `AccessControl: account ${deployerAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+        );
+        await expect(walletFactory.connect(app).updateTreasury(otherAddress)).to.be.revertedWith(
+          `AccessControl: account ${appAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+        );
+        await expect(walletFactory.connect(treasury).updateTreasury(otherAddress)).to.be.revertedWith(
+          `AccessControl: account ${treasuryAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
         );
         await expect(walletFactory.connect(other).updateTreasury(otherAddress)).to.be.revertedWith(
-          "Ownable: caller is not the owner",
+          `AccessControl: account ${otherAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+        );
+        await expect(walletFactory.connect(authorized).updateTreasury(otherAddress)).to.be.revertedWith(
+          `AccessControl: account ${authorizedAddress.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
         );
       });
 
       it("guards treasury against stuck state", async function () {
-        await expect(walletFactory.connect(owner).updateTreasury(ZERO_ADDRESS)).to.be.revertedWith(
+        await expect(walletFactory.connect(admin).updateTreasury(ZERO_ADDRESS)).to.be.revertedWith(
           "WalletFactory: new treasury is the zero address",
         );
       });
@@ -247,7 +296,8 @@ describe("WalletFactory Unit Tests", function () {
       beforeEach(async function () {
         const walletFactory__factory = new WalletFactory__factory(deployer);
         walletFactory = (await upgrades.deployProxy(walletFactory__factory, [
-          ownerAddress,
+          adminAddress,
+          appAddress,
           treasuryAddress,
         ])) as WalletFactory;
         await walletFactory.deployed();
@@ -262,10 +312,19 @@ describe("WalletFactory Unit Tests", function () {
         const walletAddress = getCreate2Address(walletFactory.address, identifier, bytecode);
         await expect(
           walletFactory.connect(deployer).transferFrom(walletAddress, usdFake.address, _1_000),
-        ).to.be.revertedWith("Ownable: caller is not the owner");
+        ).to.be.revertedWith(`AccessControl: account ${deployerAddress.toLowerCase()} is missing role ${APP_ROLE}`);
+        await expect(
+          walletFactory.connect(admin).transferFrom(walletAddress, usdFake.address, _1_000),
+        ).to.be.revertedWith(`AccessControl: account ${adminAddress.toLowerCase()} is missing role ${APP_ROLE}`);
+        await expect(
+          walletFactory.connect(treasury).transferFrom(walletAddress, usdFake.address, _1_000),
+        ).to.be.revertedWith(`AccessControl: account ${treasuryAddress.toLowerCase()} is missing role ${APP_ROLE}`);
         await expect(
           walletFactory.connect(other).transferFrom(walletAddress, usdFake.address, _1_000),
-        ).to.be.revertedWith("Ownable: caller is not the owner");
+        ).to.be.revertedWith(`AccessControl: account ${otherAddress.toLowerCase()} is missing role ${APP_ROLE}`);
+        await expect(
+          walletFactory.connect(authorized).transferFrom(walletAddress, usdFake.address, _1_000),
+        ).to.be.revertedWith(`AccessControl: account ${authorizedAddress.toLowerCase()} is missing role ${APP_ROLE}`);
       });
     });
   });
